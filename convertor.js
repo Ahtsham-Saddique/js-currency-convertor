9c12122305dd7dbbe9d8d4dd1ea596a00ce45462
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2025-12-25 20:58:08 +0500

    Addded functionality of flag pic dynamically changing

--- convertor.js
+++ convertor.js
@@ -1,14 +1,32 @@
 import { countryList } from "./code.js";
-const dropdown=document.querySelectorAll(".dropdown select")
 
+const amountInput = document.querySelector("#amount");
+  const msg = document.querySelector(".msg");
+  const form = document.querySelector("form");
+  const dropdowns = document.querySelectorAll(".dropdown select");
 
-for (let select of dropdown)
-{
-for (let curcode in countryList)
-    {   
-         let newOption=document.createElement("option");
-        newOption.innerText=curcode;
-         newOption.value=curcode;
-         select.append(newOption);
+// Populate dropdowns and flags
+  for (let select of dropdowns) {
+    for (let curcode in countryList) {
+       let newOption = document.createElement("option");
+      newOption.innerText = curcode;
+         newOption.value = curcode;
 
-    }}
+    if (curcode === "USD" && select.name === "from") newOption.selected = true;
+        if (curcode === "PKR" && select.name === "to") newOption.selected = true;
+
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (e) => {
+        updateFlag(e.target);
+        convertCurrency(); // auto-convert when currency changes
+    });
+}
+
+const updateFlag = (element) => {
+    let curcode = element.value;
+    let countrycode = countryList[curcode];
+    let img = element.parentElement.querySelector("img");
+    img.src = `https://flagsapi.com/${countrycode}/shiny/64.png`;
+};
